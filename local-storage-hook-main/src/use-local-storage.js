79680ddb8ce96eca36c4c75936eec8ca5344f006
milindpawar007 <milindpawar5555@gmail.com>
2025-05-15 14:37:54 -0500

    updated code

--- local-storage-hook-main/src/use-local-storage.js
+++ local-storage-hook-main/src/use-local-storage.js
@@ -1,5 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 export default function useLocalStorage(keyName, initialValue) {
-  // Add your solution here
-  return useState([]);
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = localStorage.getItem(keyName);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.warn("Error reading localStorage", error);
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    try {
+      localStorage.setItem(keyName, JSON.stringify(storedValue));
+    } catch (error) {
+      console.warn("Error writing to localStorage", error);
+    }
+  }, [keyName, storedValue]);
+
+  return [storedValue, setStoredValue];
 }
