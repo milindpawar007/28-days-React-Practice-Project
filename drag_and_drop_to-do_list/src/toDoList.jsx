428a044557046f2024ea119b717ae786e6a6dde3
milindpawar007 <milindpawar5555@gmail.com>
2025-05-20 15:09:59 -0500

    feat: enhance ToDoList component with input handling and task categorization

--- drag_and_drop_to-do_list/src/toDoList.jsx
+++ drag_and_drop_to-do_list/src/toDoList.jsx
@@ -1,21 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function ToDoList({todos}) {
-    
-  return (
-    <div>
-        <h2>To Do List</h2>
-        <input type="text" placeholder="Add a new task" />
-        <button>Add</button>    
-        <ul>
-            {todos.map((todo, index) => (
-            <li key={index}>
-                {todo.text}
-            </li>
-            ))}
-        </ul>
-    </div>
-  )
+function ToDoList({ todos }) {
+    const [input, setInput] = useState('');
+    const [todo, setTodo] = useState(todos);
+
+    React.useEffect(() => {
+        setTodo(todos);
+    }, [todos]);
+    const handleInputChange = (e) => {
+        setInput(e.target.value);
+    };
+
+    const handleAdd = () => {
+        const obj = { id: Date.now(), text: input, status: "to-do" };
+        setTodo(p => [...p, obj]);
+        setInput('');
+    }
+    return (
+        <>
+            <div className="todo-input-section">
+                <h2>To Do List</h2>
+                <input type="text" value={input || ''} placeholder="Add a new task" onChange={handleInputChange} />
+                <button onClick={handleAdd}>Add</button>
+            </div>
+            <div className='Container'>
+                <div className='todo'>
+                    <h2>To Do List</h2>
+                    <ul>
+                        {todo.map((todo, index) => (
+                            <li key={todo.id}>
+                                {todo.text}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className='todo'>
+                    <h2>In-Progress</h2>
+                    <ul>
+                        {todo.map((todo, index) => (
+                            <li key={todo.id}>
+                                {todo.text}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className='todo'>
+                    <h2>Done</h2>
+                    <ul>
+
+                        {todo.map((todo, index) => (
+                            <li key={todo.id}>
+                                {todo.text}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default ToDoList
