90d4dc62eda2ed7567fbcb60d36b9e4f158d8c8b
milindpawar007 <milindpawar5555@gmail.com>
2025-05-22 11:40:59 -0500

    new added

---
+++ load-more-pokemons-main/src/pokemon-list.jsx
@@ -0,0 +1,12 @@
+// You can retrieve the pokemons by calling the following API
+// Make sure to replace limit and offset with the appropriate values
+// https://pokeapi.co/api/v2/pokemon?limit=5&offset=0
+
+const PokemonList = () => {
+
+    return(
+        <h1>helllo</h1>
+    )
+};
+
+export default PokemonList;
